2829a71fbe4b6209ae836474fa74bca7ebba7e66
cpscott1 <thacam18@gmail.com>
2020-02-26 17:52:10 -0600

    adding update functionality

--- src/Todo.js
+++ src/Todo.js
@@ -3,19 +3,46 @@ import { Komponent , render} from 'kofujs'
 class Todo extends Komponent {
   constructor(props) {
     super(props)
+    this.state = {
+      isEditing: false
+    }
     this.handleRemove = this.handleRemove.bind(this)
+    this.toggleForm = this.toggleForm.bind(this)
   }
   handleRemove() {
     this.props.deleteTodo(this.props.id)
   }
+
+  toggleForm() {
+    this.setState({
+      isEditing: !this.state.isEditing
+    })
+  }
+
+  updateHandler(event) {
+
+  }
   present() {
-    return (
-      <div>
-        <button>Edit</button>
-        <button onClick={this.handleRemove}>Delete</button>
-        <li>{this.props.task}</li>
-      </div>
-    )
+    let result;
+    if(this.state.isEditing) {
+      result = (
+        <div>
+          <form onSubmit={this.updateHandler}>
+            <input type="text" />
+            <button>Save</button>
+          </form>
+        </div>
+      )
+    } else {
+      result = (
+        <div>
+          <button onClick={this.toggleForm}>Edit</button>
+          <button onClick={this.handleRemove}>Delete</button>
+          <li>{this.props.task}</li>
+        </div>
+      )
+    }
+    return result;
   }
 }
 

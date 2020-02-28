4e8c22b79ae54e9f6ad61c87df9f41bfcca0af15
cpscott1 <thacam18@gmail.com>
2020-02-27 19:27:18 -0600

    add more styling for forms

--- src/TodoForm.js
+++ src/TodoForm.js
@@ -5,6 +5,38 @@ class TodoForm extends Komponent {
     super(props)
     this.state = { task: ''}
 
+    const styles = {
+      newTodoForm: {
+        marginTop: '3rem',
+        display: 'flex',
+        flexWrap: 'wrap'
+      },
+      newTodoInput: {
+        flexGrow: '1',
+        border: 'none',
+        backgroundColor: '#f7f1f1',
+        padding: '0 1.5em',
+        fontSize: 'initial',
+        outline: 'none'
+      },
+      newTodoButton: {
+        padding: '0 1.3rem',
+        border: 'none',
+        backgroundColor: 'rgba(255, 255, 255, 0.2)',
+        color: '#fff',
+        textTransform: 'uppercase',
+        fontWeight: 'bold',
+        border: '1px solid rgba(255, 255, 255, 0.3)',
+        marginLeft: '5px',
+        cursor: 'pointer',
+        transition: 'background 0.4 ease-out',
+        outline: 'none'
+      }
+    }
+
+    const { classes } = this.setStyles(styles)
+    this.classes = classes
+
     this.inputHandler = this.inputHandler.bind(this)
     this.submitHandler = this.submitHandler.bind(this)
   }
@@ -24,14 +56,14 @@ class TodoForm extends Komponent {
 
   present(props) {
     return (
-    <form onSubmit={this.submitHandler}>
-      <input
+    <form className={this.classes.newTodoForm} onSubmit={this.submitHandler}>
+      <input className={this.classes.newTodoInput}
       placeholder="new task"
       value={this.state.task}
       id='task'
       name="task"
       onChange={this.inputHandler}/>
-      <button>Add Todo</button>
+      <button className={this.classes.newTodoButton}>Add Todo</button>
     </form>
     )
   }
